3e311c5ec37a56ac6e52481abec310682e94c44b
Daan Manneke <daan.manneke@postcodeloterij.nl>
2025-02-04 13:42:06 +0100

    break early

--- advent_of_code_2025/blog_post/aoc.py
+++ advent_of_code_2025/blog_post/aoc.py
@@ -1,26 +1,27 @@
 def solve(page):
     total = 0
     rules = {} 
-    parsing_rules = True # the start of the page contains the rules
+    parsing_rules = True
     for line in page.splitlines():                                          
 
         if line == "":
             parsing_rules = False
-            continue # skip the whiteline
+            continue
 
         if parsing_rules:
             x, y = line.split("|")
             rules[x] = rules.get(x, []) + [y]
-
         else: 
             update = line.split(",")
-            valid_update = True
             for i in range(len(update)):
                 for j in range(i + 1, len(update)):
                     if update[j] not in rules.get(update[i]):
-                        valid_update = False
-
-            if valid_update:
-                total += int(update[len(update) // 2])                     
+                        break  # break inner loop when invalid pair found
+                else:
+                    continue  # if inner loop completed without break, continue outer loop
+                break  # if inner loop broke, break outer loop too
+            else:
+                # if outer loop completed without break, update is valid
+                total += int(update[len(update) // 2])
     return total
 
